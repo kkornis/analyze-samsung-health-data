dd1b714156f980ca52aac0910750392d6d888e9a
Kornis Kristof <kokract@gmail.com>
2023-07-22 19:14:12 +0200

    Create childclass for exercise.

--- main.py
+++ main.py
@@ -87,6 +87,29 @@ class HealthDataTable:
     def get_formatted_df(self) -> pd.DataFrame:
         raise NotImplementedError
 
+    @staticmethod
+    def get_json_data(hash_data, data_type):
+        file_path = HealthDataTable.get_json_file_name(hash_data, data_type)
+        df = pd.read_json(file_path)
+        return df
+
+    @staticmethod
+    def get_complex_json_data(hash_data, data_type):
+        file_path = HealthDataTable.get_json_file_name(hash_data, data_type)
+        with open(file_path, 'r') as data_file:
+            data = json.load(data_file)
+            in_data = data['data'][0]
+            advanced_metrics = in_data['advanced_metrics']
+            df_data = {'data_type': [], 'overall_score': [], 'score_ratio': []}
+            keys = df_data.keys()
+            dfl = []
+            for line in advanced_metrics:
+                for key in keys:
+                    df_data[key].append(line[key])
+                dfl.append(pd.DataFrame(line['chart_data']))
+            df = pd.DataFrame(df_data)
+            return dfl, df, in_data['sampling_rate'], in_data['service_name']
+
 
 class SleepCombined(HealthDataTable):
     csv_data_name = 'sleep_combined'
@@ -113,9 +136,6 @@ class Sleep(HealthDataTable):
 
     def __init__(self, data_dir: str):
         super().__init__(data_dir)
-
-    def get_formatted_df(self) -> pd.DataFrame:
-        return self.get_df()
 
     def get_formatted_df(self):
         df = self.get_data()
@@ -159,94 +179,75 @@ class Sleep(HealthDataTable):
         return df
 
 
-def get_exercise_table():
-    data_name = 'exercise'
-    df = get_data(data_name)
-    cols = df.columns
-    dates_cols = ['s.e.start_time', 's.e.end_time']
-    meaningful_cols = ['total_calorie', 'heart_rate_sample_count', 's.e.duration', 's.e.exercise_type',
-                       's.e.min_altitude', 's.e.max_altitude', 's.e.mean_heart_rate', 's.e.count', 's.e.distance',
-                       's.e.calorie', 's.e.mean_speed', 's.e.altitude_gain', 's.e.sweat_loss', 's.e.min_heart_rate',
-                       's.e.max_heart_rate', 's.e.max_speed', 's.e.mean_cadence', 's.e.max_cadence',
-                       's.e.decline_distance', 's.e.vo2_max', 's.e.incline_distance', 'source_type', 'reward_status',
-                       's.e.count_type']
-    semi_meaningful_cols = ['s.e.comment']
-    meaningless_col = []
-    empty_cols = ['mission_value', 'subset_data', 'routine_datauuid', 'pace_info_id', 'pace_live_data']
-    id_cols = ['live_data_internal', 'sensing_status', 'location_data_internal', 'additional_internal',
-               's.e.location_data', 's.e.live_data']
-    col_types = [dates_cols, meaningful_cols, semi_meaningful_cols, meaningless_col, empty_cols, id_cols]
-    remaining = [rem for rem in cols if not any([rem in x for x in col_types])]
-    col_types = col_types + [remaining]
-    cols = sum(col_types, [])
-    df = df[cols]
-    for col in meaningful_cols + meaningless_col:
-        df[col] = pd.to_numeric(df[col])
-    for col in dates_cols:
-        df[col] = pd.to_datetime(df[col])
-    df.sort_values(by='s.e.start_time', inplace=True)
-    return df
+class Exercise(HealthDataTable):
+    csv_data_name = 'exercise'
 
+    def __init__(self, data_dir: str):
+        super().__init__(data_dir)
 
-def get_json_data(hash_data, data_type):
-    file_path = get_json_file_name(hash_data, data_type)
-    df = pd.read_json(file_path)
-    return df
+    def get_formatted_df(self):
+        df = self.get_data()
+        cols = df.columns
+        dates_cols = ['s.e.start_time', 's.e.end_time']
+        meaningful_cols = ['total_calorie', 'heart_rate_sample_count', 's.e.duration', 's.e.exercise_type',
+                           's.e.min_altitude', 's.e.max_altitude', 's.e.mean_heart_rate', 's.e.count', 's.e.distance',
+                           's.e.calorie', 's.e.mean_speed', 's.e.altitude_gain', 's.e.sweat_loss', 's.e.min_heart_rate',
+                           's.e.max_heart_rate', 's.e.max_speed', 's.e.mean_cadence', 's.e.max_cadence',
+                           's.e.decline_distance', 's.e.vo2_max', 's.e.incline_distance', 'source_type', 'reward_status',
+                           's.e.count_type']
+        semi_meaningful_cols = ['s.e.comment']
+        meaningless_col = []
+        empty_cols = ['mission_value', 'subset_data', 'routine_datauuid', 'pace_info_id', 'pace_live_data']
+        id_cols = ['live_data_internal', 'sensing_status', 'location_data_internal', 'additional_internal',
+                   's.e.location_data', 's.e.live_data']
+        col_types = [dates_cols, meaningful_cols, semi_meaningful_cols, meaningless_col, empty_cols, id_cols]
+        remaining = [rem for rem in cols if not any([rem in x for x in col_types])]
+        col_types = col_types + [remaining]
+        cols = sum(col_types, [])
+        df = df[cols]
+        for col in meaningful_cols + meaningless_col:
+            df[col] = pd.to_numeric(df[col])
+        for col in dates_cols:
+            df[col] = pd.to_datetime(df[col])
+        df.sort_values(by='s.e.start_time', inplace=True)
+        return df
 
+    def play_with_exercise_data(self):
+        # mean_speed is in m/s
+        df = self.get_formatted_df()
+        type_map = {1001: 'walk', 1002: 'run', 10005: 'pull_ups', 11007: 'cycling', 13001: 'hike', 14001: 'swim_outdoor',
+                    15002: 'weight_machines', 10004: 'push_up', 6003: 'badminton', 4005: 'handball'}
+        df_walk = df[df['s.e.exercise_type'] == 1001]
+        df_run = df[df['s.e.exercise_type'] == 1002]
+        df_pull_ups = df[df['s.e.exercise_type'] == 10005]
+        df_cycling = df[df['s.e.exercise_type'] == 11007]
+        df_hike = df[df['s.e.exercise_type'] == 13001]
+        df_swim_outdoor = df[df['s.e.exercise_type'] == 14001]
+        df_weight_machines = df[df['s.e.exercise_type'] == 15002]
+        df_push_up = df[df['s.e.exercise_type'] == 10004]
+        df_badminton = df[df['s.e.exercise_type'] == 6003]
+        df_handball = df[df['s.e.exercise_type'] == 4005]
 
-def get_complex_json_data(hash_data, data_type):
-    file_path = get_json_file_name(hash_data, data_type)
-    with open(file_path, 'r') as data_file:
-        data = json.load(data_file)
-        in_data = data['data'][0]
-        advanced_metrics = in_data['advanced_metrics']
-        df_data = {'data_type': [], 'overall_score': [], 'score_ratio': []}
-        keys = df_data.keys()
-        dfl = []
-        for line in advanced_metrics:
-            for key in keys:
-                df_data[key].append(line[key])
-            dfl.append(pd.DataFrame(line['chart_data']))
-        df = pd.DataFrame(df_data)
-        return dfl, df, in_data['sampling_rate'], in_data['service_name']
+        df_run['s.e.mean_speed_kmph'] = df_run['s.e.mean_speed'] * 3.6
+        df_run['s.e.max_speed_kmh'] = df_run['s.e.max_speed'] * 3.6 - df_run['s.e.mean_speed_kmph']
 
+        df_run['s.e.start_time_date'] = df_run['s.e.start_time'].dt.date
+        # df_run.plot(kind='bar', x='s.e.start_time_date', y='s.e.max_speed_kmh')
+        df_plot = df_run[['s.e.mean_speed_kmph', 's.e.max_speed_kmh', 's.e.start_time_date']]
+        df_plot.set_index('s.e.start_time_date', inplace=True)
+        df_plot.plot.bar(stacked=True)
 
-def play_with_exercise_data():
-    # mean_speed is in m/s
-    df = get_exercise_table()
-    type_map = {1001: 'walk', 1002: 'run', 10005: 'pull_ups', 11007: 'cycling', 13001: 'hike', 14001: 'swim_outdoor',
-                15002: 'weight_machines', 10004: 'push_up', 6003: 'badminton', 4005: 'handball'}
-    df_walk = df[df['s.e.exercise_type'] == 1001]
-    df_run = df[df['s.e.exercise_type'] == 1002]
-    df_pull_ups = df[df['s.e.exercise_type'] == 10005]
-    df_cycling = df[df['s.e.exercise_type'] == 11007]
-    df_hike = df[df['s.e.exercise_type'] == 13001]
-    df_swim_outdoor = df[df['s.e.exercise_type'] == 14001]
-    df_weight_machines = df[df['s.e.exercise_type'] == 15002]
-    df_push_up = df[df['s.e.exercise_type'] == 10004]
-    df_badminton = df[df['s.e.exercise_type'] == 6003]
-    df_handball = df[df['s.e.exercise_type'] == 4005]
+        line_ind = 0
 
-    df_run['s.e.mean_speed_kmph'] = df_run['s.e.mean_speed'] * 3.6
-    df_run['s.e.max_speed_kmh'] = df_run['s.e.max_speed'] * 3.6 - df_run['s.e.mean_speed_kmph']
+        data1 = self.get_json_data(df_run['live_data_internal'].iloc[line_ind], 's.exercise')
+        data2 = self.get_json_data(df_run['sensing_status'].iloc[line_ind], 's.exercise')
+        data3 = self.get_json_data(df_run['location_data_internal'].iloc[line_ind], 's.exercise')
+        data4 = self.get_complex_json_data(df_run['additional_internal'].iloc[line_ind], 's.exercise')
+        data5 = self.get_json_data(df_run['s.e.location_data'].iloc[line_ind], 's.exercise')
+        data6 = self.get_json_data(df_run['s.e.live_data'].iloc[line_ind], 's.exercise')
+        # data7 = get_json_data(df_run['s.e.datauuid'].iloc[line_ind] + '.heart_rate.json', True)
 
-    df_run['s.e.start_time_date'] = df_run['s.e.start_time'].dt.date
-    # df_run.plot(kind='bar', x='s.e.start_time_date', y='s.e.max_speed_kmh')
-    df_plot = df_run[['s.e.mean_speed_kmph', 's.e.max_speed_kmh', 's.e.start_time_date']]
-    df_plot.set_index('s.e.start_time_date', inplace=True)
-    df_plot.plot.bar(stacked=True)
-
-    line_ind = 0
-
-    data1 = get_json_data(df_run['live_data_internal'].iloc[line_ind], 's.exercise')
-    data2 = get_json_data(df_run['sensing_status'].iloc[line_ind], 's.exercise')
-    data3 = get_json_data(df_run['location_data_internal'].iloc[line_ind], 's.exercise')
-    data4 = get_complex_json_data(df_run['additional_internal'].iloc[line_ind], 's.exercise')
-    data5 = get_json_data(df_run['s.e.location_data'].iloc[line_ind], 's.exercise')
-    data6 = get_json_data(df_run['s.e.live_data'].iloc[line_ind], 's.exercise')
-    # data7 = get_json_data(df_run['s.e.datauuid'].iloc[line_ind] + '.heart_rate.json', True)
-
-    return df
+        return df
 
 
 def get_heart_rate_table():
@@ -288,10 +289,13 @@ def play_with_heart_rate_data():
 
 def main():
     direct = os.path.join(os.path.dirname(__file__), 'data')
+
     sleep = Sleep(direct)
     sleep.play_with_sleep_data()
-    # play_with_sleep_data()
-    # play_with_exercise_data()
+
+    exercise = Exercise(direct)
+    exercise.play_with_exercise_data()
+
     # play_with_heart_rate_data()
 
     plt.show()
